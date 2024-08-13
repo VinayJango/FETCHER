a5a8c8bef31cb8174c412023b12662efcb5e7769
Vinay jangam <84473308+VinayJango@users.noreply.github.com>
2024-08-13 14:43:40 +0530

    fetcher_core.py | Added Core File !

---
+++ fetcher_core.py
@@ -0,0 +1,10 @@
+import pyfiglet
+from rich import print
+
+def fcb():
+    title = pyfiglet.figlet_format('FETCHER', font='slant')
+    print(f'[green]{title}[/green]')
+
+
+def fcc():
+    print("[+]Tool Developed By Vinay jangam \n[+]Github- https://github.com/Vinayjango")
